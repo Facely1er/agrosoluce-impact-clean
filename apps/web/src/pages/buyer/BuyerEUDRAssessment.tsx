1739e852fb17bf33a275eebfa2383176bdc0512c
Facely1er <facelykande@gmail.com>
2026-02-21 02:26:38 -0500

    refactor: Remove unused import in BuyerEUDRAssessment component

--- apps/web/src/pages/buyer/BuyerEUDRAssessment.tsx
+++ apps/web/src/pages/buyer/BuyerEUDRAssessment.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useRef, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
   ArrowLeft,
   ArrowRight,
